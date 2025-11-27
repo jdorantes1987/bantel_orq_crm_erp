94bd97c1e9a48c5a0889ad2ec5b9f3cf2baeef51
jdorantes1987 <165170008+jdorantes1987@users.noreply.github.com>
2025-11-27 17:33:59 -0400

    Refactor client number.

--- clients/clientes_profit.py
+++ clients/clientes_profit.py
@@ -11,29 +11,29 @@ class ClientesMonitoreoProfit:
         self.c_engine = db.conn_engine()
         self.oClientes = Clientes(db)
         self.counter_num_cliente = 0
+        self._last_num_cliente = 0
 
     def obtener_clientes_activos(self):
         """Obtiene una lista de clientes activos."""
         data = self.oClientes.get_clientes_profit()
         return data[data["inactivo"] == 0]
 
-    def _set_numero_cliente(self, number):
-        self.counter_num_cliente += 1
-        return str(number + self.counter_num_cliente).zfill(0)
+    def _set_numero_cliente(self):
+        self._last_num_cliente += 1
+        return str(self._last_num_cliente).zfill(0)
 
-    def get_next_num_recibo(self, number) -> str:
-        return "CL" + self._set_numero_cliente(number)
+    def next_cod_cliente(self) -> str:
+        return "CL" + self._set_numero_cliente()
 
-    def get_last_cod_cliente(self) -> int:
+    def get_new_cod_cliente(self) -> str:
         sql = """select MAX(TRY_PARSE(SUBSTRING(co_cli,PATINDEX('%[0-9]%',co_cli),LEN(co_cli)) AS INT)) as num_co_cli
                  from saCliente
                  where tipo_adi=1
             """
         cur = self.db.execute(sql, ())
-        result = cur.fetchone()
-        if not result:
-            return 0
-        return result[0]
+        num_max = cur.fetchone()
+        self._last_num_cliente = num_max[0] if num_max else 0
+        return self.next_cod_cliente()
 
 
 if __name__ == "__main__":
@@ -68,11 +68,10 @@ if __name__ == "__main__":
     sqlserver_connector.connect()
     db_derecha = DatabaseConnector(sqlserver_connector)
     oClientesMonitoreo = ClientesMonitoreoProfit(db_derecha)
-    last_number = oClientesMonitoreo.get_last_cod_cliente()
-    new_cod_cliente = oClientesMonitoreo.get_next_num_recibo(last_number)
+    last_number = oClientesMonitoreo.get_new_cod_cliente()
+    print(last_number)
+    new_cod_cliente = oClientesMonitoreo.next_cod_cliente()
     print(new_cod_cliente)
-    new_cod_cliente = oClientesMonitoreo.get_next_num_recibo(last_number)
-    print(new_cod_cliente)
-    new_cod_cliente = oClientesMonitoreo.get_next_num_recibo(last_number)
+    new_cod_cliente = oClientesMonitoreo.next_cod_cliente()
     print(new_cod_cliente)
     db_derecha.close_connection()
